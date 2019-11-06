4a5be0257d5f88b4498a4cbd2d81745316168efd
zhongji <featherfly@foxmail.com>
2019-11-06 18:49:50 +0800

    c

--- juorm-sqldb/src/test/java/cn/featherfly/juorm/rdb/jdbc/dsl/SqlQueryTest.java
+++ juorm-sqldb/src/test/java/cn/featherfly/juorm/rdb/jdbc/dsl/SqlQueryTest.java
@@ -32,8 +32,7 @@ public class SqlQueryTest extends JdbcTestBase {
     @Test
     void test0() {
         SqlQuery query = new SqlQuery(jdbc, metadata);
-        List<Map<String, Object>> list = query.find("user")
-                .property("username", "password", "age").sort().asc("age")
+        List<Map<String, Object>> list = query.find("user").property("username", "password", "age").sort().asc("age")
                 .list();
         int age = Integer.MIN_VALUE;
         for (Map<String, Object> map : list) {
@@ -48,36 +47,31 @@ public class SqlQueryTest extends JdbcTestBase {
     void test1() {
         SqlQuery query = new SqlQuery(jdbc, metadata);
         query.find("user").list(User.class);
-        query.find("user").property("username", "password", "age")
-                .list(User.class);
-        query.find("user").property("username", "password", "age").where()
-                .eq("username", "yufei").and().eq("password", "123456").and()
-                .group().gt("age", 18).and().lt("age", 60).list(User.class);
+        query.find("user").property("username", "password", "age").list(User.class);
+        query.find("user").property("username", "password", "age").where().eq("username", "yufei").and()
+                .eq("password", "123456").and().group().gt("age", 18).and().lt("age", 60).list(User.class);
     }
 
     @Test
     void test2() {
         SqlQuery query = new SqlQuery(jdbc, metadata);
-        query.find("user").property("username", "password", "age").where()
-                .eq("username", "yufei").and().eq("password", "123456").and()
-                .group().gt("age", 18).and().lt("age", 60).list(User.class);
+        query.find("user").property("username", "password", "age").where().eq("username", "yufei").and()
+                .eq("password", "123456").and().group().gt("age", 18).and().lt("age", 60).list(User.class);
     }
 
     @Test
     void test3() {
         SqlQuery query = new SqlQuery(jdbc, metadata);
-        query.find(new SimpleRepository("user", "u")).where()
-                .eq("username", "yufei").and().eq("password", "123456").and()
-                .group().gt("age", 18).and().lt("age", 60).list(User.class);
+        query.find(new SimpleRepository("user", "u")).where().eq("username", "yufei").and().eq("password", "123456")
+                .and().group().gt("age", 18).and().lt("age", 60).list(User.class);
     }
 
     @Test
     void test4() {
         SqlQuery query = new SqlQuery(jdbc, metadata);
-        query.find(new SimpleRepository("user", "u"))
-                .property("username", "password", "age").where()
-                .eq("username", "yufei").and().eq("password", "123456").and()
-                .group().gt("age", 18).and().lt("age", 60).list(User.class);
+        query.find(new SimpleRepository("user", "u")).property("username", "password", "age").where()
+                .eq("username", "yufei").and().eq("password", "123456").and().group().gt("age", 18).and().lt("age", 60)
+                .list(User.class);
     }
 
     @SuppressWarnings("unchecked")
@@ -85,22 +79,17 @@ public class SqlQueryTest extends JdbcTestBase {
     void testMapping() {
         SqlQuery query = new SqlQuery(jdbc, mappingFactory);
 
-        query.find(User.class).where().eq("username", "yufei").and()
-                .eq("pwd", "123456").and().group().gt("age", 18).and()
-                .lt("age", 60).list();
+        query.find(User.class).where().eq("username", "yufei").and().eq("pwd", "123456").and().group().gt("age", 18)
+                .and().lt("age", 60).list();
 
-        query.find(User.class).where().eq("username", "yufei").and()
-                .eq("pwd", "123456").and().group().gt("age", 18).and()
-                .property("age").lt(60).list();
+        query.find(User.class).where().eq("username", "yufei").and().eq("pwd", "123456").and().group().gt("age", 18)
+                .and().property("age").lt(60).list();
 
-        query.find(User.class).property("username", "pwd", "age").where()
-                .eq("username", "yufei").and().eq("pwd", "123456").and().group()
-                .gt("age", 18).and().lt("age", 60).list();
+        query.find(User.class).property("username", "pwd", "age").where().eq("username", "yufei").and()
+                .eq("pwd", "123456").and().group().gt("age", 18).and().lt("age", 60).list();
 
-        query.find(User.class)
-                .property(User::getUsername, User::getPwd, User::getAge).where()
-                .eq("username", "yufei").and().eq("pwd", "123456").and().group()
-                .gt("age", 18).and().lt("age", 60).list();
+        query.find(User.class).property(User::getUsername, User::getPwd, User::getAge).where().eq("username", "yufei")
+                .and().eq("pwd", "123456").and().group().gt("age", 18).and().lt("age", 60).list();
         /*
          * query.find(User.class).with(UserInfo.class, UserInfo::getUser)
          * query.find(UserInfo.class).with(UserInfo::getUser, User.class)
@@ -110,24 +99,20 @@ public class SqlQueryTest extends JdbcTestBase {
          * "user_role").on("id", "role_id")
          * query.find("user_info").with("user_id", "user")
          */
-        query.find(User.class)
-                .property(User::getUsername, User::getPwd, User::getAge).where()
-                .eq("username", "yufei").and().eq(User::getPwd, "123456").and()
-                .group().gt(User::getAge, 18).and().lt(User::getAge, 60).list();
+        query.find(User.class).property(User::getUsername, User::getPwd, User::getAge).where().eq("username", "yufei")
+                .and().eq(User::getPwd, "123456").and().group().gt(User::getAge, 18).and().lt(User::getAge, 60).list();
     }
 
     @Test
     void testNestedMapping() {
         SqlQuery query = new SqlQuery(jdbc, mappingFactory);
         Integer userId = 1;
-        UserInfo userInfo = query.find(UserInfo.class).where()
-                .eq("user.id", userId).single();
+        UserInfo userInfo = query.find(UserInfo.class).where().eq("user.id", userId).single();
         assertEquals(userInfo.getUser().getId(), userId);
         System.out.println(userInfo);
 
         String province = "四川";
-        userInfo = query.find(UserInfo.class).where()
-                .eq("division.province", province).single();
+        userInfo = query.find(UserInfo.class).where().eq("division.province", province).single();
         assertEquals(userInfo.getDivision().getProvince(), province);
         System.out.println(userInfo);
     }
@@ -136,66 +121,52 @@ public class SqlQueryTest extends JdbcTestBase {
     void testJoin() {
         SqlQuery query = new SqlQuery(jdbc, mappingFactory);
 
-        query.find("user").property("username", "password", "age")
-                .with("user_info").on("user_id").list();
+        query.find("user").property("username", "password", "age").with("user_info").on("user_id").list();
 
-        query.find("user").property("username", "password", "age")
-                .with("user_info").on("user_id").fetch("name").list();
+        query.find("user").property("username", "password", "age").with("user_info").on("user_id").fetch("name").list();
 
-        query.find("user").property("username", "password", "age")
-                .with("user_info").on("user_id").fetch().list();
+        query.find("user").property("username", "password", "age").with("user_info").on("user_id").fetch().list();
 
-        query.find("user").property("username", "password", "age")
-                .with("user_info").on("user_id").fetch("name").list();
+        query.find("user").property("username", "password", "age").with("user_info").on("user_id").fetch("name").list();
 
-        query.find("user").property("username", "password", "age")
-                .with("user_info").on("user_id").fetch("name").fetch().list();
-
-        query.find("user").property("username", "password", "age")
-                .with("user_info").on("user_id").fetch("name").fetch("descp")
+        query.find("user").property("username", "password", "age").with("user_info").on("user_id").fetch("name").fetch()
                 .list();
 
-        query.find("user").property("username", "password", "age")
-                .with("user_role").on("user_id").with("role")
+        query.find("user").property("username", "password", "age").with("user_info").on("user_id").fetch("name")
+                .fetch("descp").list();
+
+        query.find("user").property("username", "password", "age").with("user_role").on("user_id").with("role")
                 .on("id", "user_role", "role_id").fetch().list();
 
-        query.find("user").property("username", "password", "age")
-                .with(UserInfo.class).on("user_id").list();
+        query.find("user").property("username", "password", "age").with(UserInfo.class).on("user_id").list();
 
-        query.find("user").property("username", "password", "age")
-                .with(UserInfo.class).on("user_id").fetch().list();
+        query.find("user").property("username", "password", "age").with(UserInfo.class).on("user_id").fetch().list();
 
         query.find("tree").with("tree").on("parent_id").list();
 
-        query.find("tree").with("tree").on("parent_id").with("tree")
-                .on("parent_id").list();
+        query.find("tree").with("tree").on("parent_id").with("tree").on("parent_id").list();
 
-        query.find("user_info").with("user").on("id", "user_id")
-                .fetchAlias("password", "pwd").fetch().list();
+        query.find("user_info").with("user").on("id", "user_id").fetchAlias("password", "pwd").fetch().list();
     }
 
     @Test
     void testJoinCondition() {
         SqlQuery query = new SqlQuery(jdbc, mappingFactory);
 
-        List<Map<String, Object>> list = query.find("user")
-                .property("username", "password", "age").with("user_info")
+        List<Map<String, Object>> list = query.find("user").property("username", "password", "age").with("user_info")
                 .on("user_id").where().eq("user_info", "name", "羽飞").list();
         assertEquals(list.size(), 1);
 
-        list = query.find("user").property("username", "password", "age")
-                .with("user_info").on("user_id").where().eq(1, "name", "羽飞")
-                .list();
+        list = query.find("user").property("username", "password", "age").with("user_info").on("user_id").where()
+                .eq(1, "name", "羽飞").list();
         assertEquals(list.size(), 1);
 
-        list = query.find("user").property("username", "password", "age")
-                .with("user_info").on("user_id").where()
+        list = query.find("user").property("username", "password", "age").with("user_info").on("user_id").where()
                 .property("user_info", "name").eq("羽飞").list();
         assertEquals(list.size(), 1);
 
-        list = query.find("user").property("username", "password", "age")
-                .with("user_info").on("user_id").where().property(1, "name")
-                .eq("羽飞").list();
+        list = query.find("user").property("username", "password", "age").with("user_info").on("user_id").where()
+                .property(1, "name").eq("羽飞").list();
         assertEquals(list.size(), 1);
     }
 
@@ -203,22 +174,18 @@ public class SqlQueryTest extends JdbcTestBase {
     void testJoin2() {
         SqlQuery query = new SqlQuery(jdbc, mappingFactory);
 
-        UserInfo userInfo = query.find(UserInfo.class).with(UserInfo::getUser)
-                .where().eq(UserInfo::getId, 1).single();
+        UserInfo userInfo = query.find(UserInfo.class).with(UserInfo::getUser).where().eq(UserInfo::getId, 1).single();
         System.err.println(userInfo);
         assertNull(userInfo.getUser().getUsername());
-        userInfo = query.find(UserInfo.class).with(UserInfo::getUser).fetch()
-                .where().eq(UserInfo::getId, 1).single();
+        userInfo = query.find(UserInfo.class).with(UserInfo::getUser).fetch().where().eq(UserInfo::getId, 1).single();
         assertNotNull(userInfo.getUser().getUsername());
         System.err.println(userInfo);
 
-        query.find(UserInfo.class).with(UserInfo::getUser)
-                .with(UserRole2::getUser).with(UserRole2::getRole).list();
+        query.find(UserInfo.class).with(UserInfo::getUser).with(UserRole2::getUser).with(UserRole2::getRole).list();
 
         query.find(Tree2.class).with(Tree2::getParent).list();
 
-        query.find(Tree2.class).with(Tree2::getParent).with(Tree2::getParent, 1)
-                .list();
+        query.find(Tree2.class).with(Tree2::getParent).with(Tree2::getParent, 1).list();
     }
 
     @Test
@@ -228,28 +195,22 @@ public class SqlQueryTest extends JdbcTestBase {
         Integer userId = 1;
         User user = null;
 
-        user = query.find(User.class).with(UserInfo::getUser).where()
-                .eq(1, "id", userInfoId).single();
+        user = query.find(User.class).with(UserInfo::getUser).where().eq(1, "id", userInfoId).single();
         assertEquals(user.getId(), userId);
 
-        user = query.find(User.class).with(UserInfo::getUser).where()
-                .eq("user_info", "id", userInfoId).single();
+        user = query.find(User.class).with(UserInfo::getUser).where().eq("user_info", "id", userInfoId).single();
         assertEquals(user.getId(), userId);
 
-        user = query.find(User.class).with(UserInfo::getUser).where()
-                .eq(UserInfo.class, "id", userInfoId).single();
+        user = query.find(User.class).with(UserInfo::getUser).where().eq(UserInfo.class, "id", userInfoId).single();
         assertEquals(user.getId(), userId);
 
-        user = query.find(User.class).with(UserInfo::getUser).where()
-                .eq(User::getId, userId).single();
+        user = query.find(User.class).with(UserInfo::getUser).where().eq(User::getId, userId).single();
         assertEquals(user.getId(), userId);
 
-        user = query.find(User.class).with(UserInfo::getUser).where()
-                .eq(UserInfo::getId, userInfoId).single();
+        user = query.find(User.class).with(UserInfo::getUser).where().eq(UserInfo::getId, userInfoId).single();
         assertEquals(user.getId(), userId);
 
-        user = query.find(User.class).with(UserInfo::getUser).where()
-                .property(UserInfo::getId).eq(userInfoId).single();
+        user = query.find(User.class).with(UserInfo::getUser).where().property(UserInfo::getId).eq(userInfoId).single();
         assertEquals(user.getId(), userId);
     }
 
@@ -262,30 +223,26 @@ public class SqlQueryTest extends JdbcTestBase {
             System.err.println(v);
         });
 
-        List<Tree2> list2 = query.find(Tree2.class).with(Tree2::getParent)
-                .list();
+        List<Tree2> list2 = query.find(Tree2.class).with(Tree2::getParent).list();
         list2.forEach(v -> {
             assertNotNull(v.getParent().getId());
             System.err.println(v);
         });
 
-        list2 = query.find(Tree2.class).with(Tree2::getParent)
-                .with(Tree2::getParent).list();
+        list2 = query.find(Tree2.class).with(Tree2::getParent).with(Tree2::getParent).list();
         list2.forEach(v -> {
             assertNotNull(v.getParent().getId());
             System.err.println(v);
         });
 
-        List<Tree2> list3 = query.find(Tree2.class).with(Tree2::getParent)
-                .fetch().list();
+        List<Tree2> list3 = query.find(Tree2.class).with(Tree2::getParent).fetch().list();
         list3.forEach(v -> {
             assertNotNull(v.getParent().getId());
             assertNotNull(v.getParent().getName());
             System.err.println(v);
         });
 
-        list3 = query.find(Tree2.class).with(Tree2::getParent).fetch()
-                .with(Tree2::getParent, 1).fetch().list();
+        list3 = query.find(Tree2.class).with(Tree2::getParent).fetch().with(Tree2::getParent, 1).fetch().list();
         list3.forEach(v -> {
             assertNotNull(v.getParent().getId());
             assertNotNull(v.getParent().getName());
