83e3f10c74093ae274ff77c55956876491884a69
featherfly <featherfly@foxmail.com>
2020-04-15 01:59:04 +0800

    加入SqldbHammer接口

---
+++ hammer-sqldb/src/main/java/cn/featherfly/hammer/sqldb/SqldbHammer.java
@@ -0,0 +1,21 @@
+
+package cn.featherfly.hammer.sqldb;
+
+import cn.featherfly.hammer.Hammer;
+import cn.featherfly.hammer.sqldb.jdbc.Jdbc;
+
+/**
+ * <p>
+ * SqldbHammer
+ * </p>
+ *
+ * @author zhongj
+ */
+public interface SqldbHammer extends Hammer {
+    /**
+     * get Jdbc
+     *
+     * @return jdbc
+     */
+    Jdbc getJdbc();
+}
