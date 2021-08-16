a7f135dc0d471cec82b88b92ae22e7a863de4c32
zhongji <featherfly@foxmail.com>
2021-08-16 15:21:05 +0800

    修复预编译优化条件标签<@and>和<@Or>，在编译为Freemarker模板后，明确加入name="fieldName"属性时CO,SW,EW丢失的问题

--- hammer-core/src/main/java/cn/featherfly/hammer/tpl/processor/Parser.java
+++ hammer-core/src/main/java/cn/featherfly/hammer/tpl/processor/Parser.java
@@ -203,14 +203,14 @@ public class Parser {
                                                 append = " && " + name + "?length gt 0";
                                             }
                                         }
-                                        append = append + " name=\"" + name + "\"";
+
                                         boolean endWith = paramContent
                                                 .charAt(namePart.getStart() - 2) == fuzzyQueryChar;
                                         boolean startWith = paramContent.charAt(namePart.getEnd()) == fuzzyQueryChar;
                                         append = appendTransverter(endWith, startWith, append);
 
                                         //                                        de.setSource(pre + name + de.getSource() + append);
-                                        de.setSource(pre + name + de.getSource() + " name=\"" + name + "\"");
+                                        de.setSource(pre + name + de.getSource() + append + " name=\"" + name + "\"");
 
                                     } else {
                                         name = de.getSource().replaceAll("\\?", "");
