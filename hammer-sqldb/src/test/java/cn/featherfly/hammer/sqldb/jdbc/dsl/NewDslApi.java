20c9023a355daf8a27089d88909497e11c252799
featherfly <featherfly@foxmail.com>
2021-09-28 02:05:37 +0800

    doc: 添加下一步功能实现的验证代码文件

---
+++ hammer-sqldb/src/test/java/cn/featherfly/hammer/sqldb/jdbc/dsl/NewDslApi.java
@@ -0,0 +1,63 @@
+
+package cn.featherfly.hammer.sqldb.jdbc.dsl;
+
+import java.util.List;
+import java.util.function.Function;
+
+import com.speedment.common.tuple.Tuple;
+import com.speedment.common.tuple.Tuple2;
+import com.speedment.common.tuple.Tuples;
+
+import cn.featherfly.hammer.expression.Repository;
+import cn.featherfly.hammer.expression.SimpleRepository;
+import cn.featherfly.hammer.sqldb.jdbc.dsl.query.SqlQuery;
+import cn.featherfly.hammer.sqldb.jdbc.vo.User;
+import cn.featherfly.hammer.sqldb.jdbc.vo.UserInfo;
+
+/**
+ * NewDslApi.
+ *
+ * @author zhongj
+ */
+public class NewDslApi {
+
+    public static void main(String[] args) {
+
+        SqlQuery query = null;
+
+        UserInfo userInfo = new UserInfo();
+
+        List<UserInfo> userInfos = query.find(UserInfo.class).with(UserInfo::getUser).where()
+                .eq(userInfo::getUser, User::getPwd).list();
+
+        // 实例对象可以获取property
+        query.find(UserInfo.class).where().eq(userInfos.get(0)::getDescp);
+        query.find(UserInfo.class).where().eq(userInfo::getDescp);
+
+        Function<FindAndFetchManager<Tuple2<EntityRepository<UserInfo>, EntityRepository<UserInfo>>>, Repository> f = (
+                find) -> {
+            return find.getFindAndFetch().get0();
+        };
+
+        Function<Tuple2<EntityRepository<UserInfo>, EntityRepository<User>>, Repository> f2 = (tuple) -> {
+            return tuple.get0();
+        };
+
+        f2.apply(Tuples.of(new EntityRepository<UserInfo>(), new EntityRepository<User>()));
+    }
+}
+
+class EntityRepository<T> extends SimpleRepository {
+    public Class<T> type() {
+        return null;
+    }
+}
+
+class FindAndFetchManager<T extends Tuple> {
+
+    private T tuple;
+
+    public T getFindAndFetch() {
+        return tuple;
+    }
+}
