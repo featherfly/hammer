120f7df96714d322c9552bd2a4740a24160be1a5
zhongji <featherfly@foxmail.com>
2022-08-04 16:53:45 +0800

    feat: dsl条件查询eq(SerialFunction<T, R>, T),ne(SerialFunction<T, R> t)方法支持@Embedded对象，自动使用该对象的所有非空属性

--- hammer-api/src/main/java/cn/featherfly/hammer/tpl/Transverter.java
+++ hammer-api/src/main/java/cn/featherfly/hammer/tpl/Transverter.java
@@ -1,22 +1,27 @@
-///*
-// * All rights Reserved, Designed By zhongj
-// * @Title: Transverter.java
-// * @Package cn.featherfly.hammer.sqldb.tpl
-// * @Description: Transverter
-// * @author: zhongj
-// * @date: 2021-08-17 14:53:17
-// * @Copyright: 2021 www.featherfly.cn Inc. All rights reserved.
-// */
-//package cn.featherfly.hammer.tpl;
-//
-//import cn.featherfly.common.repository.operate.Operator;
-//
-///**
-// * transvert param value before use.
-// *
-// * @author zhongj
-// */
-//public interface Transverter {
-//
-//    Object transvert(Operator operator, Object value);
-//}
+/*
+ * All rights Reserved, Designed By zhongj
+ * @Title: Transverter.java
+ * @Package cn.featherfly.hammer.sqldb.tpl
+ * @Description: Transverter
+ * @author: zhongj
+ * @date: 2021-08-17 14:53:17
+ * @Copyright: 2021 www.featherfly.cn Inc. All rights reserved.
+ */
+package cn.featherfly.hammer.tpl;
+
+/**
+ * transvert param value before use.
+ *
+ * @author zhongj
+ */
+public interface Transverter {
+
+    /**
+     * Transvert.
+     *
+     * @param operator the operator
+     * @param value    the value
+     * @return the object
+     */
+    Object transvert(String operator, Object value);
+}
