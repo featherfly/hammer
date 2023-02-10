44504b3894a7c06ee565088016ba8c933c167591
zhongj <featherfly@foxmail.com>
2023-02-10 18:16:07 +0800

    refactor: 重命名测试文件

---
+++ hammer-sqldb/src/test/java/cn/featherfly/hammer/sqldb/jdbc/HammerJdbcTemplateTest.java
@@ -0,0 +1,23 @@
+
+package cn.featherfly.hammer.sqldb.jdbc;
+
+import org.testng.annotations.BeforeClass;
+
+import cn.featherfly.hammer.sqldb.SqldbHammerImpl;
+import cn.featherfly.hammer.sqldb.tpl.SqlTplExecutorTest;
+import cn.featherfly.hammer.sqldb.tpl.freemarker.SqldbFreemarkerTemplateEngine;
+
+/**
+ * HammerJdbcTest.
+ *
+ * @author zhongj
+ */
+public class HammerJdbcTemplateTest extends SqlTplExecutorTest {
+
+    @BeforeClass
+    void before() {
+        //        TplConfigFactoryImpl configFactory = new TplConfigFactoryImpl("tpl/", ".yaml.tpl");
+        executor = new SqldbHammerImpl(jdbc, mappingFactory, configFactory,
+                new SqldbFreemarkerTemplateEngine(configFactory), new SimpleSqlPageFactory());
+    }
+}
